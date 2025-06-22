17b674efedaa389a101563bf40deb8043bc3b0fc
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-06-22 18:58:37 -0400

    yaml

--- powershell-scripts/install-scripts/install-powershell-7.ps1
+++
@@ -1,1 +0,0 @@
-winget install --id Microsoft.PowerShell --source winget --scope machine --silent --accept-package-agreements --accept-source-agreements
--- powershell-scripts/install-scripts/install-vs-code.ps1
+++
@@ -1,1 +0,0 @@
-winget install -e --id Microsoft.VisualStudioCode --scope machine --silent --accept-package-agreements --accept-source-agreements
